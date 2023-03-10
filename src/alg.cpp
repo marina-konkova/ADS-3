8388cc3ec21ce401ef1a897cbc98e14f51e6ea57
marina-konkova <116936403+marina-konkova@users.noreply.github.com>
2023-03-10 12:56:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int count = 0;
+  int leftborder = 0;
+  int rightborder = size - 1;
+  while (leftborder <= rightborder) {
+    int seredina = (leftborder + rightborder) / 2;
+    if (arr[seredina] == value) {            
+      count++;
+      int i = seredina - 1;
+      while (i >= leftborder && arr[i] == value) {
+        count++;
+        i--;
+      }
+      i = seredina + 1;
+      while (i <= rightborder && arr[i] == value) {
+        count++;
+        i++;
+      }
+      break;
+    } else if (arr[seredina] < value) {
+      leftborder = seredina + 1;
+    } else {
+      rightborder = seredina - 1;
+    }
+  }
+  return count;
 }
